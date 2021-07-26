25ebf063d308404fad205aa68e34651523d6b395
awesomecoder233 <tommount58@gmail.com>
2021-07-27 01:20:10 +0200

    add search component

---
+++ resources/js/components/searchComponent.vue
@@ -0,0 +1,33 @@
+<template>
+
+<div class="card">
+
+  <div class="card-header" id="hide">
+    <div class="row">
+       <div class="col-md-2">
+          <input type="text" @keyup="searchUnit" placeholder="Search" v-model="search" class="form-control form-control-sm">
+        </div>
+    </div>
+  </div>
+
+
+  <div class="card-body" id="hide_again">
+    <table id="" class="table table-bordered table-striped">
+      <thead>
+      <tr>
+         <th>Name</th>
+         <th>Status</th>
+       </tr>
+      </thead>
+     <tbody>
+        <tr v-for="unit in units.data" :key="unit.id">
+          <td>{{ unit.name }}</td>
+          <td>{{ unit.is_active == 1 ? 'Active' : 'InActive' }}</td>
+        </tr>
+     </tbody>
+     <pagination :data="units" @pagination-change-page="getResults"></pagination>
+    </table>
+
+  </div>
+</div>
+</template>
